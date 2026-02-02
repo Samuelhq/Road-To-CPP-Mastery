2817279e25712c79f77484039865526be08881b4
Samuelhq <thesamokonkwo@gmail.com>
2026-02-02 20:02:31 +0100

    Arrays Basics

---
+++ Module 2/Module 2/arrays_basics.cpp
@@ -0,0 +1,14 @@
+/*This simple program explores the Static Arrays*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int myNumbers[5] = { 99, 234, -47, 29, 38 };
+	cout << "First Number is: " << myNumbers[0] << endl;
+	cout << "Second Number is: " << myNumbers[1] << endl;
+	cout << "Third Number is: " << myNumbers[2] << endl;
+	cout << "Fourth Number is: " << myNumbers[3] << endl;
+	cout << "Fifth Number is: " << myNumbers[4] << endl;
+	return 0;
+}
